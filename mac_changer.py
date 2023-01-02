ade2e4a12cb68da14b3b4fa19769984a306e4dda
SrBlackRM <michelrbm@gmail.com>
2023-01-02 13:45:46 -0300

    Add files via upload

--- mac_changer.py
+++ mac_changer.py
@@ -2,6 +2,7 @@
 
 import subprocess
 import optparse
+import re
 
 def shellcommand(text):
     subprocess.call(text, shell=True)
@@ -10,23 +11,9 @@ def change_mac(interface, mac):
     shellcommand('ifconfig ' + interface + ' down')
     shellcommand('ifconfig ' + interface + ' hw ether ' + mac)
     shellcommand('ifconfig ' + interface + ' up')
-    print("\n\n[+] Changing MAC Address for " + interface + " to " + mac)
-    print("\n done!")
+    print("\n[+] Changing MAC Address for " + interface)
 
-def show_result():
-    answ = str(input("\nwant to see results? (y/n): "))
-    if answ == "":
-        answ = "n"
-        exit(0)
-    elif answ == "y":
-        shellcommand("ifconfig")
-    elif answ == "n":
-        print("\nByeee!")
-        exit(0)
-    else:
-        print("Error")
-
-def get_arguments():       
+def get_arguments():
     parser = optparse.OptionParser()
     parser.add_option("-i", "--interface", dest="interface", help="interface to change its MAC Address")
     parser.add_option("-m", "--mac", dest="new_mac", help="what MAC do you want to change")
@@ -37,8 +24,28 @@ def get_arguments():
         parser.error("\n\n[-] Please specify a Mac Address, use --help for more info!")
     return options
 
+def get_current_mac(interface):
+    ifconfig_result = subprocess.check_output(['ifconfig',interface])
+    mac_address_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", str(ifconfig_result))
+
+    if mac_address_result:
+        return mac_address_result.group(0)
+    else:
+        print("\n[-] Could not read Mac Address")
+
 options = get_arguments()
+current_mac = get_current_mac(options.interface)
+
+print("\nCurrent MAC: "+str(current_mac))
+if not current_mac: 
+    exit(0)
+
 change_mac(options.interface, options.new_mac)
-show_result()
+current_mac = get_current_mac(options.interface)
+
+if current_mac == options.new_mac:
+    print("\n[+] MAC Address has successfully changed to: " + current_mac)
+else:
+    print("\n[-] MAC Address did not changed. ")
 
 # 34:45:56:67:78:89
